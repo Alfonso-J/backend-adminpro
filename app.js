433b711b0f9945cf87a16f714b1665781d03e69e
Alfonso Baena <ajbaena@gmail.com>
2018-04-21 20:14:37 +0200

    Primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+var app = express();
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
+})
+
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log('Express ejecutandose en el puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
