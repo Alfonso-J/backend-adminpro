dac4681cbc47aef26f3ff7d2e8e46aaddcc10d80
Alfonso Baena <ajbaena@gmail.com>
2018-05-01 14:03:57 +0200

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     if (err) throw err;
@@ -12,14 +22,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 })
 
-app.get('/', (req, res, next) => {
+app.use('/login', loginRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
 
 app.listen(3000, () => {
     console.log('Express ejecutandose en el puerto 3000: \x1b[32m%s\x1b[0m', 'online');
